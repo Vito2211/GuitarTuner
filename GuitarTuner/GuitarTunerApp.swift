725c0c7c9a6f5c93d2fb5b7343142cff809d1ab4
Viktor Pavliuk <mypc@MacBook-Air-Viktor-Pavliuk.local>
2026-07-12 22:12:37 +0200

    Initial commit

---
+++ GuitarTuner/GuitarTunerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuitarTunerApp.swift
+//  GuitarTuner
+//
+//  Created by  Viktor Pavliuk on 12.07.2026.
+//
+
+import SwiftUI
+
+@main
+struct GuitarTunerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GuitarTunerTests/GuitarTunerTests.swift
@@ -0,0 +1,17 @@
+//
+//  GuitarTunerTests.swift
+//  GuitarTunerTests
+//
+//  Created by  Viktor Pavliuk on 12.07.2026.
+//
+
+import Testing
+@testable import GuitarTuner
+
+struct GuitarTunerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
